f15ed4d5598df59e771f13dda865b54a914e96c7
Daniel Haendel <daniel.haendel@tbs.students.de>
2024-10-28 16:00:24 +0100

    adding a card and the js logic for the answers...

---
+++ static/js/db.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        "question": "Welches Protokoll wird häufig für den Versand von Webseiten verwendet?",
+        "option1": "FTP",
+        "option2": "SMTP",
+        "option3": "HTTP",
+        "option4": "SSH",
+        "answer": 3
+    },
+    {
+        "question": "Wofür steht das 'C' in CSS?",
+        "option1": "Color",
+        "option2": "Cascading",
+        "option3": "Code",
+        "option4": "Clear",
+        "answer": 2
+    },
+    {
+        "question": "Welche Programmiersprache wird häufig für dynamische Inhalte in Webanwendungen verwendet?",
+        "option1": "Python",
+        "option2": "JavaScript",
+        "option3": "Java",
+        "option4": "C++",
+        "answer": 2
+    },
+    {
+        "question": "Welche dieser HTML-Tags dient zur Darstellung von Bildern?",
+        "option1": "&lt;div&gt;",
+        "option2": "&lt;p&gt;",
+        "option3": "&lt;img&gt;",
+        "option4": "&lt;table&gt;",
+        "answer": 3
+    },
+    {
+        "question": "Mit welchem Attribut lässt sich ein Hyperlink in einem neuen Tab öffnen?",
+        "option1": "open='_new'",
+        "option2": "window='_blank'",
+        "option3": "target='_newtab'",
+        "option4": "target='_blank'",
+        "answer": 4
+    },
+    {
+        "question": "Wie heißt die Markup-Sprache, die auf XML basiert und häufig für Webseiten verwendet wird?",
+        "option1": "JSON",
+        "option2": "HTML",
+        "option3": "CSS",
+        "option4": "Markdown",
+        "answer": 2
+    },
+    {
+        "question": "Wofür steht das 'S' in HTTPS?",
+        "option1": "Secure",
+        "option2": "Socket",
+        "option3": "Service",
+        "option4": "Standard",
+        "answer": 1
+    },
+    {
+        "question": "Welches HTML-Tag wird für einen Absatz verwendet?",
+        "option1": "&lt;p&gt;",
+        "option2": "&lt;div&gt;",
+        "option3": "&lt;span&gt;",
+        "option4": "&lt;title&gt;",
+        "answer": 1
+    },
+    {
+        "question": "Welcher dieser Selektoren in CSS spricht alle Elemente an?",
+        "option1": "*",
+        "option2": "#",
+        "option3": ".",
+        "option4": "all",
+        "answer": 1
+    },
+    {
+        "question": "Welche Eigenschaft wird in CSS verwendet, um die Hintergrundfarbe zu ändern?",
+        "option1": "color",
+        "option2": "font-color",
+        "option3": "background",
+        "option4": "background-color",
+        "answer": 4
+    }
+];
---
+++ static/js/scripts.js
@@ -0,0 +1,42 @@
+function init() {
+    showQuestion();
+}
+
+let currentQuestion = 0;
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    document.getElementById("question").innerText = question.question;
+    document.getElementById("answer-1").innerText = question.option1;
+    document.getElementById("answer-2").innerText = question.option2;
+    document.getElementById("answer-3").innerText = question.option3;
+    document.getElementById("answer-4").innerText = question.option4;
+    document.getElementById("quiz-length").innerHTML = questions.length;
+    document.getElementById("current-question").innerHTML = currentQuestion + 1;
+    document.getElementById("next-button").disabled = true;
+}
+
+function answer(option){
+    let question = questions[currentQuestion];
+    if(option == question.answer){
+        document.getElementById("answer-" + option).classList.add("correct");
+    } else {
+        document.getElementById("answer-" + option).classList.add("wrong");
+        document.getElementById("answer-" + question.answer).parentNode.classList.add("correct-highlight");
+    }
+    document.getElementById("next-button").disabled = false;
+}
+
+function nextQuestion (){
+    currentQuestion++;
+    cleanUp();
+    showQuestion();
+}
+
+function cleanUp(){
+    for (let i = 1; i <= 4; i++){
+        document.getElementById("answer-" + i).classList.remove("correct");
+        document.getElementById("answer-" + i).classList.remove("wrong");
+        document.getElementById("answer-" + i).parentNode.classList.remove("correct-highlight");
+    }
+}
